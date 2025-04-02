723d922edf7c4db0e776bd537b078987404343ac
MandaliaSPG <sofiapg.2002@hotmail.com>
2025-04-02 07:53:38 -0700

    Fin Seccion 26

--- 11-Websockets/src/app.ts
+++ 11-Websockets/src/app.ts
@@ -10,12 +10,21 @@ wss.on('connection', function connection(ws) {
 
     ws.on('message', function message(data) {
 
-        console.log('received: %s', data);
-        const payload={
+        const payload=JSON.stringify({
             type:'custom-message',
             payload: data.toString()
-        }
-        ws.send(JSON.stringify(payload))
+        })
+        //ws.send(JSON.stringify(payload))
+
+        wss.clients.forEach(function each(client) {
+            // if (client.readyState === WebSocket.OPEN) {  //Esta madre es para hacer un chat :00000
+            //   client.send(payload, { binary: false });
+            // }
+
+            if(client !== ws && client.readyState=== WebSocket.OPEN){ //Esto sirve como chat tambien pero no puedes mirar tus propios mensajes 
+                client.send(payload, {binary: false})
+            }
+          });
     });
 
     ws.send('sandia');
